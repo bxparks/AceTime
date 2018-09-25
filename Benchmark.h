dcd1625a3e0740ba06788cb999cbe6dca278d96c
Brian Park <brian@xparks.net>
2018-09-24 22:29:50 -0700

    Extract examples/ComparisonBenchmark to move to GitHub repo

--- Benchmark.h
+++
@@ -1,8 +0,0 @@
-#if defined(LED_BUILTIN)
-  #define LED_BENCHMARK LED_BUILTIN
-#else
-  // define a random pin number for those boards that have multiple BUILTIN
-  #define LED_BENCHMARK 5
-#endif
-
-extern void runBenchmarks();
