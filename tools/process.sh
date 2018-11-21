c48a9e2f259d586b9c64506f08040561f1f7cf98
Brian Park <brian@xparks.net>
2018-11-21 10:45:12 -0600

    tools/process.sh: add version to shell script, use git to check out the given tag

--- tools/process.sh
+++ tools/process.sh
@@ -12,4 +12,24 @@ DIRNAME=$(dirname $0)
 # Point to the github repository.
 DB_DIR=$HOME/dev/tz
 
-$DIRNAME/process.py --input_dir $DB_DIR "$@"
+function usage() {
+    echo 'Usage: process.sh version'
+    exit 1
+}
+
+
+if [[ $# -ne 1 ]]; then
+    usage
+fi
+tz_version=$1
+shift
+
+pushd $DB_DIR
+git co $tz_version
+popd
+
+$DIRNAME/process.py --input_dir $DB_DIR --tz_version $tz_version "$@"
+
+pushd $DB_DIR
+git co master
+popd
