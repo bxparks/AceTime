1ae25c143cc30e1287b61a500136ffb9178893c0
Brian Park <brian@xparks.net>
2018-12-26 21:41:08 -0800

    test_extractor.py: Fix minor broken test for untilTimeModifier from recent refactoring

--- tools/test_extractor.py
+++ tools/test_extractor.py
@@ -10,7 +10,7 @@ from extractor import parse_at_time_string
 
 class TestParseAtHourString(unittest.TestCase):
     def test_parse_at_time_string(self):
-        self.assertEqual(('2:00', 'w'), parse_at_time_string('2:00'))
+        self.assertEqual(('2:00', ''), parse_at_time_string('2:00'))
         self.assertEqual(('2:00', 'w'), parse_at_time_string('2:00w'))
         self.assertEqual(('12:00', 's'), parse_at_time_string('12:00s'))
         self.assertEqual(('12:00', 'g'), parse_at_time_string('12:00g'))
