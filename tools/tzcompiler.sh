157b901961eaf16aa41eecedf8fc2c4196f4b8a9
Brian Park <brian@xparks.net>
2019-01-15 18:01:52 -0800

    tools: Add documentation of examples how to use tzcompiler.sh

--- tools/tzcompiler.sh
+++ tools/tzcompiler.sh
@@ -3,6 +3,41 @@
 # Copyright 2018 Brian T. Park
 #
 # MIT License
+#
+# Usage:
+#
+#   $ tzcompiler.sh --tag 2018i --python --granularity 60
+#       - generates zone*.py files in the current directory
+#
+#   $ tzcompiler.sh --tag 2018i --arduino --granularity 900
+#       - generates zone*.{h,cpp} files in the current directory
+#
+#   $ tzcompiler.sh --tag 2018i --validate --granularity 60
+#       - validate the internal zone_info and zone_policies data
+#
+#   $ tzcompiler.sh --tag 2018i --validate --granularity 60 --optimized
+#       - validate the internal zone_info and zone_policies data
+# Flags
+#
+#   Transformer flags:
+#
+#       --start_year
+#           Retain TZ information since this year (default 2000).
+#       --granularity
+#           Retain time value fields in seconds (default 900)
+#       --strict
+#           Remove zone and rules not aligned at time granularity.
+#
+#   Validator:
+#       --validate
+#           Validate the zone_infos and zone_policies
+#       --optimized
+#           Use a 14-month window, instead of a 3 year window.
+#       --validate_dst_offset
+#           Validate DST offsets as well (many false positives due to pytz).
+#       --validate_hours
+#           Validate 0h to 23h of each day from 2000 to 2038, instead of
+#           just the transition days. (Takes long time).
 
 set -eu
 
