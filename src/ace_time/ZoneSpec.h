b2c8b704ae705e8d574122b1800a93406ed94c0c
Brian Park <brian@xparks.net>
2019-01-25 09:04:41 -0800

    src: Add doc for ZoneSpec to explain that using virtual methods increases program size

--- src/ace_time/ZoneSpec.h
+++ src/ace_time/ZoneSpec.h
@@ -6,6 +6,21 @@
 
 namespace ace_time {
 
+/**
+ * Base interface for ZoneSpec classes. This contains just the getType()
+ * discriminator distinguish between the 2 implementation classes
+ * (ManualZoneSpec and AutoZoneSpec). The TimeZone class will use the
+ * discriminator to determine which of the 2 subclasses to use at runtime.
+ *
+ * An alternative design was to lift various common methods (getUtcOffset(),
+ * getDeltaOffset(), getAbbrev()) into this interface as virtual methods, then
+ * add a virtual equals() method to implement the operator==(). I thought that
+ * if the application used only AutoZoneSpec or ManualZoneSpec (but not both),
+ * the compiler might be able to reduce the program code size by removing the
+ * code for the unused class. However, in reality, this alternative design
+ * caused the program size to *increase* by 200-300 bytes. I'm not exactly sure
+ * why but that was the reality.
+ */
 class ZoneSpec {
   public:
     static const uint8_t kTypeManual = 0;
