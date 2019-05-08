2fac2f04e3b61fcf5ca4e32eff869b4d7e81eee1
Brian Park <brian@xparks.net>
2019-05-07 19:25:06 -0700

    Fix Jenkins continuous integration by excluding BasicValidationTest and ExtendedValidationTest

--- examples/WorldClock/config.h
+++ examples/WorldClock/config.h
@@ -39,7 +39,7 @@
 #define TIME_ZONE_TYPE_MANUAL 0
 #define TIME_ZONE_TYPE_BASIC 1
 #define TIME_ZONE_TYPE_EXTENDED 2
-#define TIME_ZONE_TYPE TIME_ZONE_TYPE_EXTENDED
+#define TIME_ZONE_TYPE TIME_ZONE_TYPE_BASIC
 
 //------------------------------------------------------------------
 // Rendering modes.
