3f86ece5ccce8ae60106662b5f0324867b15e3f2
Brian Park <brian@xparks.net>
2019-05-10 09:08:10 -0700

    tests: Extract TransitionStorageTest from ExtendedZoneSpecifierTest to fit an Arduino ProMicro

---
+++ tests/TransitionStorageTest/TransitionStorageTest.ino
@@ -0,0 +1,291 @@
+#line 2 "TransitionStorageTest.ino"
+
+/*
+ * Unit tests for TransitionStorage, extracted from ExtendedZoneSpecifierTest
+ * after it became too big to run on an Arduino/SparkFun ProMicro environment.
+ */
+
+#include <AUnit.h>
+#include <AceTime.h>
+
+using namespace aunit;
+using namespace ace_time;
+using namespace ace_time::zonedbx;
+
+test(TransitionStorageTest, getFreeAgent) {
+  TransitionStorage<4> storage;
+  storage.init();
+
+  Transition* freeAgent = storage.getFreeAgent();
+  assertTrue(freeAgent == &storage.mPool[0]);
+  storage.addFreeAgentToActivePool();
+
+  freeAgent = storage.getFreeAgent();
+  assertTrue(freeAgent == &storage.mPool[1]);
+  storage.addFreeAgentToActivePool();
+
+  freeAgent = storage.getFreeAgent();
+  assertTrue(freeAgent == &storage.mPool[2]);
+  storage.addFreeAgentToActivePool();
+
+  freeAgent = storage.getFreeAgent();
+  assertTrue(freeAgent == &storage.mPool[3]);
+  storage.addFreeAgentToActivePool();
+
+  // Verify overflow checking.
+  freeAgent = storage.getFreeAgent();
+  assertTrue(freeAgent == &storage.mPool[3]);
+}
+
+test(TransitionStorageTest, getFreeAgent2) {
+  TransitionStorage<4> storage;
+  storage.init();
+
+  Transition* freeAgent = storage.getFreeAgent();
+  assertTrue(freeAgent == storage.mTransitions[0]);
+  storage.addFreeAgentToCandidatePool();
+  assertEqual(1, storage.mIndexFree);
+
+  freeAgent = storage.getFreeAgent();
+  assertTrue(freeAgent == storage.mTransitions[1]);
+  storage.addFreeAgentToCandidatePool();
+  assertEqual(2, storage.mIndexFree);
+
+  freeAgent = storage.getFreeAgent();
+  assertTrue(freeAgent == storage.mTransitions[2]);
+  storage.addFreeAgentToCandidatePool();
+  assertEqual(3, storage.mIndexFree);
+
+  freeAgent = storage.getFreeAgent();
+  assertTrue(freeAgent == storage.mTransitions[3]);
+  storage.addFreeAgentToCandidatePool();
+  assertEqual(4, storage.mIndexFree);
+
+  // Verify overflow checking.
+  freeAgent = storage.getFreeAgent();
+  assertTrue(freeAgent == storage.mTransitions[3]);
+}
+
+test(TransitionStorageTest, addFreeAgentToActivePool) {
+  TransitionStorage<4> storage;
+  storage.init();
+
+  Transition* freeAgent = storage.getFreeAgent();
+  assertTrue(freeAgent == &storage.mPool[0]);
+
+  storage.addFreeAgentToActivePool();
+  assertEqual(1, storage.mIndexPrior);
+  assertEqual(1, storage.mIndexCandidates);
+  assertEqual(1, storage.mIndexFree);
+}
+
+test(TransitionStorageTest, reservePrior) {
+  TransitionStorage<4> storage;
+  storage.init();
+  Transition** prior = storage.reservePrior();
+  assertTrue(prior == &storage.mTransitions[0]);
+  assertEqual(0, storage.mIndexPrior);
+  assertEqual(1, storage.mIndexCandidates);
+  assertEqual(1, storage.mIndexFree);
+
+  storage.addPriorToCandidatePool();
+  assertEqual(0, storage.mIndexPrior);
+  assertEqual(0, storage.mIndexCandidates);
+  assertEqual(1, storage.mIndexFree);
+}
+
+test(TransitionStorageTest, setFreeAgentAsPrior) {
+  TransitionStorage<4> storage;
+  storage.init();
+
+  Transition** priorReservation = storage.reservePrior();
+  (*priorReservation)->active = false;
+  Transition* freeAgent = storage.getFreeAgent();
+  freeAgent->active = true;
+  storage.setFreeAgentAsPrior();
+
+  // Verify that the two have been swapped.
+  Transition* prior = storage.getPrior();
+  freeAgent = storage.getFreeAgent();
+  assertTrue(prior->active);
+  assertFalse(freeAgent->active);
+}
+
+test(TransitionStorageTest, addFreeAgentToCandidatePool) {
+  TransitionStorage<4> storage;
+  storage.init();
+
+  // create Prior to make it interesting
+  /*Transition* prior =*/ storage.reservePrior();
+
+  // Verify that addFreeAgentToCandidatePool() does not touch prior transition
+  Transition* freeAgent = storage.getFreeAgent();
+  freeAgent->transitionTime = {0, 1, 2, 3, 'w'};
+  storage.addFreeAgentToCandidatePool();
+  assertEqual(0, storage.mIndexPrior);
+  assertEqual(1, storage.mIndexCandidates);
+  assertEqual(2, storage.mIndexFree);
+
+  freeAgent = storage.getFreeAgent();
+  freeAgent->transitionTime = {2, 3, 4, 5, 'w'};
+  storage.addFreeAgentToCandidatePool();
+  assertEqual(0, storage.mIndexPrior);
+  assertEqual(1, storage.mIndexCandidates);
+  assertEqual(3, storage.mIndexFree);
+
+  freeAgent = storage.getFreeAgent();
+  freeAgent->transitionTime = {1, 2, 3, 4, 'w'};
+  storage.addFreeAgentToCandidatePool();
+  assertEqual(0, storage.mIndexPrior);
+  assertEqual(1, storage.mIndexCandidates);
+  assertEqual(4, storage.mIndexFree);
+
+  // Assert that the transitions are sorted
+  assertEqual(0, storage.getTransition(1)->transitionTime.yearTiny);
+  assertEqual(1, storage.getTransition(2)->transitionTime.yearTiny);
+  assertEqual(2, storage.getTransition(3)->transitionTime.yearTiny);
+}
+
+test(TransitionStorageTest, addActiveCandidatesToActivePool) {
+  TransitionStorage<4> storage;
+  storage.init();
+
+  // create Prior to make it interesting
+  Transition** prior = storage.reservePrior();
+  (*prior)->transitionTime = {-1, 0, 1, 2, 'w'};
+  (*prior)->active = true;
+
+  // Add 3 transitions to Candidate pool, 2 active, 1 inactive.
+  Transition* freeAgent = storage.getFreeAgent();
+  freeAgent->transitionTime = {0, 1, 2, 3, 'w'};
+  freeAgent->active = true;
+  storage.addFreeAgentToCandidatePool();
+
+  freeAgent = storage.getFreeAgent();
+  freeAgent->transitionTime = {2, 3, 4, 5, 'w'};
+  freeAgent->active = true;
+  storage.addFreeAgentToCandidatePool();
+
+  freeAgent = storage.getFreeAgent();
+  freeAgent->transitionTime = {1, 2, 3, 4, 'w'};
+  freeAgent->active = false;
+  storage.addFreeAgentToCandidatePool();
+
+  // Add prior into the Candidate pool.
+  storage.addPriorToCandidatePool();
+
+  // Add the actives to the Active pool.
+  storage.addActiveCandidatesToActivePool();
+
+  // Verify that there are 3 transitions in the Active pool.
+  assertEqual(3, storage.mIndexPrior);
+  assertEqual(3, storage.mIndexCandidates);
+  assertEqual(3, storage.mIndexFree);
+  assertEqual(-1, storage.getTransition(0)->transitionTime.yearTiny);
+  assertEqual(0, storage.getTransition(1)->transitionTime.yearTiny);
+  assertEqual(2, storage.getTransition(2)->transitionTime.yearTiny);
+}
+
+test(TransitionStorageTest, findTransition) {
+  TransitionStorage<4> storage;
+  storage.init();
+
+  // Add 3 transitions to Candidate pool, 2 active, 1 inactive.
+  Transition* freeAgent = storage.getFreeAgent();
+  freeAgent->transitionTime = {0, 1, 2, 3, 'w'};
+  freeAgent->active = true;
+  freeAgent->startEpochSeconds = 0;
+  storage.addFreeAgentToCandidatePool();
+
+  freeAgent = storage.getFreeAgent();
+  freeAgent->transitionTime = {1, 2, 3, 4, 'w'};
+  freeAgent->active = true;
+  freeAgent->startEpochSeconds = 10;
+  storage.addFreeAgentToCandidatePool();
+
+  freeAgent = storage.getFreeAgent();
+  freeAgent->transitionTime = {2, 3, 4, 5, 'w'};
+  freeAgent->active = true;
+  freeAgent->startEpochSeconds = 20;
+  storage.addFreeAgentToCandidatePool();
+
+  // Add the actives to the Active pool.
+  storage.addActiveCandidatesToActivePool();
+
+  // Check that we can find the transitions using the startEpochSeconds.
+
+  const Transition* t = storage.findTransition(1);
+  assertEqual(0, t->transitionTime.yearTiny);
+
+  t = storage.findTransition(9);
+  assertEqual(0, t->transitionTime.yearTiny);
+
+  t = storage.findTransition(10);
+  assertEqual(1, t->transitionTime.yearTiny);
+
+  t = storage.findTransition(21);
+  assertEqual(2, t->transitionTime.yearTiny);
+}
+
+test(TransitionStorageTest, resetCandidatePool) {
+  TransitionStorage<4> storage;
+  storage.init();
+
+  // Add 2 transitions to Candidate pool, 2 active, 1 inactive.
+  Transition* freeAgent = storage.getFreeAgent();
+  freeAgent->transitionTime = {0, 1, 2, 3, 'w'};
+  freeAgent->active = true;
+  storage.addFreeAgentToCandidatePool();
+  assertEqual(0, storage.mIndexPrior);
+  assertEqual(0, storage.mIndexCandidates);
+  assertEqual(1, storage.mIndexFree);
+
+  freeAgent = storage.getFreeAgent();
+  freeAgent->transitionTime = {2, 3, 4, 5, 'w'};
+  freeAgent->active = true;
+  storage.addFreeAgentToCandidatePool();
+  assertEqual(0, storage.mIndexPrior);
+  assertEqual(0, storage.mIndexCandidates);
+  assertEqual(2, storage.mIndexFree);
+
+  // Add active candidates to Active pool. Looks like this
+  // already does a resetCandidatePool() effectively.
+  storage.addActiveCandidatesToActivePool();
+  assertEqual(2, storage.mIndexPrior);
+  assertEqual(2, storage.mIndexCandidates);
+  assertEqual(2, storage.mIndexFree);
+
+  // This should be a no-op.
+  storage.resetCandidatePool();
+  assertEqual(2, storage.mIndexPrior);
+  assertEqual(2, storage.mIndexCandidates);
+  assertEqual(2, storage.mIndexFree);
+
+  freeAgent = storage.getFreeAgent();
+  freeAgent->transitionTime = {1, 2, 3, 4, 'w'};
+  freeAgent->active = false;
+  storage.addFreeAgentToCandidatePool();
+  assertEqual(2, storage.mIndexPrior);
+  assertEqual(2, storage.mIndexCandidates);
+  assertEqual(3, storage.mIndexFree);
+
+  // Reset should remove any remaining candidate transitions.
+  storage.resetCandidatePool();
+  assertEqual(2, storage.mIndexPrior);
+  assertEqual(2, storage.mIndexCandidates);
+  assertEqual(2, storage.mIndexFree);
+}
+
+// --------------------------------------------------------------------------
+
+void setup() {
+#if defined(ARDUINO)
+  delay(1000); // wait for stability on some boards to prevent garbage Serial
+#endif
+  Serial.begin(115200); // ESP8266 default of 74880 not supported on Linux
+  while(!Serial); // for the Arduino Leonardo/Micro only
+}
+
+void loop() {
+  TestRunner::run();
+}
