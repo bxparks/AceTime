6b16a99b06553b7877a9a235ecc8e1ea4bf41046
Brian Park <brian@xparks.net>
2019-05-15 17:51:58 -0700

    tests: Extract DateTimeMutationTest from ZonedDateTimeTest, too large for ProMicro board

---
+++ tests/DateTimeMutationTest/DateTimeMutationTest.ino
@@ -0,0 +1,81 @@
+#line 2 "DateTimeMutationTest.ino"
+
+#include <AUnit.h>
+#include <AceTime.h>
+
+using namespace aunit;
+using namespace ace_time;
+
+// --------------------------------------------------------------------------
+// data_time_mutation
+// --------------------------------------------------------------------------
+
+test(DateTimeMutationTest, increment) {
+  ZonedDateTime dt = ZonedDateTime::forComponents(2001, 2, 3, 4, 5, 6);
+  assertEqual((int16_t) 2001, dt.year());
+  assertEqual(2, dt.month());
+  assertEqual(3, dt.day());
+  assertEqual(4, dt.hour());
+  assertEqual(5, dt.minute());
+  assertEqual(6, dt.second());
+  assertEqual(0, dt.timeZone().getUtcOffset(0).toMinutes());
+
+  date_time_mutation::incrementYear(dt);
+  assertEqual((int16_t) 2002, dt.year());
+  assertEqual(2, dt.month());
+  assertEqual(3, dt.day());
+  assertEqual(4, dt.hour());
+  assertEqual(5, dt.minute());
+  assertEqual(6, dt.second());
+  assertEqual(0, dt.timeZone().getUtcOffset(0).toMinutes());
+
+  date_time_mutation::incrementMonth(dt);
+  assertEqual((int16_t) 2002, dt.year());
+  assertEqual(3, dt.month());
+  assertEqual(3, dt.day());
+  assertEqual(4, dt.hour());
+  assertEqual(5, dt.minute());
+  assertEqual(6, dt.second());
+  assertEqual(0, dt.timeZone().getUtcOffset(0).toMinutes());
+
+  date_time_mutation::incrementDay(dt);
+  assertEqual((int16_t) 2002, dt.year());
+  assertEqual(3, dt.month());
+  assertEqual(4, dt.day());
+  assertEqual(4, dt.hour());
+  assertEqual(5, dt.minute());
+  assertEqual(6, dt.second());
+  assertEqual(0, dt.timeZone().getUtcOffset(0).toMinutes());
+
+  date_time_mutation::incrementHour(dt);
+  assertEqual((int16_t) 2002, dt.year());
+  assertEqual(3, dt.month());
+  assertEqual(4, dt.day());
+  assertEqual(5, dt.hour());
+  assertEqual(5, dt.minute());
+  assertEqual(6, dt.second());
+  assertEqual(0, dt.timeZone().getUtcOffset(0).toMinutes());
+
+  date_time_mutation::incrementMinute(dt);
+  assertEqual((int16_t) 2002, dt.year());
+  assertEqual(3, dt.month());
+  assertEqual(4, dt.day());
+  assertEqual(5, dt.hour());
+  assertEqual(6, dt.minute());
+  assertEqual(6, dt.second());
+  assertEqual(0, dt.timeZone().getUtcOffset(0).toMinutes());
+}
+
+// --------------------------------------------------------------------------
+
+void setup() {
+#if !defined(__linux__) && !defined(__APPLE__)
+  delay(1000); // wait for stability on some boards to prevent garbage Serial
+#endif
+  Serial.begin(115200); // ESP8266 default of 74880 not supported on Linux
+  while(!Serial); // for the Arduino Leonardo/Micro only
+}
+
+void loop() {
+  TestRunner::run();
+}
--- tests/ZonedDateTimeTest/ZonedDateTimeTest.ino
+++ tests/ZonedDateTimeTest/ZonedDateTimeTest.ino
@@ -363,66 +363,6 @@ test(ZonedDateTimeTest, forDateString_errors) {
 }
 
 // --------------------------------------------------------------------------
-// data_time_mutation
-// --------------------------------------------------------------------------
-
-test(DateTimeMutationTest, increment) {
-  ZonedDateTime dt = ZonedDateTime::forComponents(2001, 2, 3, 4, 5, 6);
-  assertEqual((int16_t) 2001, dt.year());
-  assertEqual(2, dt.month());
-  assertEqual(3, dt.day());
-  assertEqual(4, dt.hour());
-  assertEqual(5, dt.minute());
-  assertEqual(6, dt.second());
-  assertEqual(0, dt.timeZone().getUtcOffset(0).toMinutes());
-
-  date_time_mutation::incrementYear(dt);
-  assertEqual((int16_t) 2002, dt.year());
-  assertEqual(2, dt.month());
-  assertEqual(3, dt.day());
-  assertEqual(4, dt.hour());
-  assertEqual(5, dt.minute());
-  assertEqual(6, dt.second());
-  assertEqual(0, dt.timeZone().getUtcOffset(0).toMinutes());
-
-  date_time_mutation::incrementMonth(dt);
-  assertEqual((int16_t) 2002, dt.year());
-  assertEqual(3, dt.month());
-  assertEqual(3, dt.day());
-  assertEqual(4, dt.hour());
-  assertEqual(5, dt.minute());
-  assertEqual(6, dt.second());
-  assertEqual(0, dt.timeZone().getUtcOffset(0).toMinutes());
-
-  date_time_mutation::incrementDay(dt);
-  assertEqual((int16_t) 2002, dt.year());
-  assertEqual(3, dt.month());
-  assertEqual(4, dt.day());
-  assertEqual(4, dt.hour());
-  assertEqual(5, dt.minute());
-  assertEqual(6, dt.second());
-  assertEqual(0, dt.timeZone().getUtcOffset(0).toMinutes());
-
-  date_time_mutation::incrementHour(dt);
-  assertEqual((int16_t) 2002, dt.year());
-  assertEqual(3, dt.month());
-  assertEqual(4, dt.day());
-  assertEqual(5, dt.hour());
-  assertEqual(5, dt.minute());
-  assertEqual(6, dt.second());
-  assertEqual(0, dt.timeZone().getUtcOffset(0).toMinutes());
-
-  date_time_mutation::incrementMinute(dt);
-  assertEqual((int16_t) 2002, dt.year());
-  assertEqual(3, dt.month());
-  assertEqual(4, dt.day());
-  assertEqual(5, dt.hour());
-  assertEqual(6, dt.minute());
-  assertEqual(6, dt.second());
-  assertEqual(0, dt.timeZone().getUtcOffset(0).toMinutes());
-}
-
-// --------------------------------------------------------------------------
 
 void setup() {
 #if !defined(__linux__) && !defined(__APPLE__)
