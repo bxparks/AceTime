ab45341e86022cf20c5e6e85c6e5dcca1606b35c
Brian Park <brian@xparks.net>
2019-05-16 19:21:53 -0700

    examples: Enable LedClock.ino and FullOledClock.ino to compile with ESP8266 and ESP32 using AUniter for continuous builds

--- examples/FullOledClock/config.h
+++ examples/FullOledClock/config.h
@@ -34,22 +34,22 @@ const uint8_t MODE_CHANGE_HOUR_MODE = 19;
   #warning Using default AUNITER_MICRO_MINDER
 #endif
 
-#if defined(AUNITER_MICRO_MINDER)
-  #define MODE_BUTTON_PIN 8
-  #define CHANGE_BUTTON_PIN 9
-  #define TIME_SOURCE_TYPE TIME_SOURCE_TYPE_DS3231
-  #define OLED_REMAP true
-#elif defined(AUNITER_NANO)
+#if defined(AUNITER_NANO)
   #define MODE_BUTTON_PIN 2
   #define CHANGE_BUTTON_PIN 3
   #define TIME_SOURCE_TYPE TIME_SOURCE_TYPE_DS3231
   #define OLED_REMAP false
+#elif defined(AUNITER_MICRO) || defined(AUNITER_MICRO_MINDER)
+  #define MODE_BUTTON_PIN 8
+  #define CHANGE_BUTTON_PIN 9
+  #define TIME_SOURCE_TYPE TIME_SOURCE_TYPE_DS3231
+  #define OLED_REMAP true
 #elif defined(AUNITER_MINI_MINDER)
   #define MODE_BUTTON_PIN 2
   #define CHANGE_BUTTON_PIN 3
   #define TIME_SOURCE_TYPE TIME_SOURCE_TYPE_DS3231
   #define OLED_REMAP false
-#elif defined(AUNITER_ESP_MINDER)
+#elif defined(AUNITER_ESP8266) || defined(AUNITER_ESP_MINDER)
   #define MODE_BUTTON_PIN D4
   #define CHANGE_BUTTON_PIN D3
   #define TIME_SOURCE_TYPE TIME_SOURCE_TYPE_NTP
@@ -57,6 +57,11 @@ const uint8_t MODE_CHANGE_HOUR_MODE = 19;
 #elif defined(AUNITER_ESP_MINDER2)
   #define MODE_BUTTON_PIN D4
   #define CHANGE_BUTTON_PIN D3
+  #define TIME_SOURCE_TYPE TIME_SOURCE_TYPE_NTP
+  #define OLED_REMAP true
+#elif defined(AUNITER_ESP32)
+  #define MODE_BUTTON_PIN 4
+  #define CHANGE_BUTTON_PIN 3
   #define TIME_SOURCE_TYPE TIME_SOURCE_TYPE_NTP
   #define OLED_REMAP true
 #else
