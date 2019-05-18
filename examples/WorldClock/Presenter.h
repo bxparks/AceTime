57c95eff82a8dbf4f9326ad5bd3da00d8812c80b
Brian Park <brian@xparks.net>
2019-05-17 23:14:27 -0700

    examples/WorldClock: Print AceButton and AceRoutine versions on About screen

--- examples/WorldClock/Presenter.h
+++ examples/WorldClock/Presenter.h
@@ -271,18 +271,7 @@ class Presenter {
 #endif
     }
 
-    void displayAbout() const {
-      mOled.setFont(SystemFont5x7);
-
-      // For smallest flash memory size, use F() macros for these longer
-      // strings, but no F() for shorter version strings.
-      mOled.print(F("WorldClock: "));
-      mOled.println(CLOCK_VERSION_STRING);
-      mOled.print(F("Tzdata: "));
-      mOled.println(zonedb::kTzDatabaseVersion);
-      mOled.print(F("AceTime: "));
-      mOled.print(ACE_TIME_VERSION_STRING);
-    }
+    void displayAbout() const;
 
     /**
      * True if the display should actually show the data. If the clock is in
