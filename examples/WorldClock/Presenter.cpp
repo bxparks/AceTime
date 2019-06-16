6bda704351da094e2fd522778704e4b2a455a17b
Brian Park <brian@xparks.net>
2019-06-15 20:14:11 -0700

    WorldClock: Remove SystemFont5x7 from version screen, reuse old font; saves 512 bytes of flash

--- examples/WorldClock/Presenter.cpp
+++ examples/WorldClock/Presenter.cpp
@@ -3,11 +3,11 @@
 #include "Presenter.h"
 
 void Presenter::displayAbout() const {
-  mOled.setFont(SystemFont5x7);
+  mOled.set1X();
 
   // For smallest flash memory size, use F() macros for these longer
   // strings, but no F() for shorter version strings.
-  mOled.print(F("AceTime: "));
+  mOled.print(F("AT: "));
   mOled.println(F(ACE_TIME_VERSION_STRING));
   mOled.print(F("TZ: "));
   mOled.println(zonedb::kTzDatabaseVersion);
