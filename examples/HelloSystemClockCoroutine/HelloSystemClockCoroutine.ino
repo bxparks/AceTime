437466ee8cc4a22619644e1515f77e2a1fd7f669
Brian Park <brian@xparks.net>
2019-06-17 20:18:35 -0700

    examples: Add HelloSystemClock and HelloSystemClockCoroutine examples

---
+++ examples/HelloSystemClockCoroutine/HelloSystemClockCoroutine.ino
@@ -0,0 +1,83 @@
+/*
+ * Identical to HelloSystemClock, but using AceRoutine coroutines.
+ * Should print the following on the Serial port every 2 seconds:
+ *
+ *   Pacific Time: 2019-06-17T19:50:00-07:00 Monday [America/Los_Angeles]
+ *   Eastern Time: 2019-06-17T22:50:00-04:00 Monday [America/New_York]
+ *
+ *   Pacific Time: 2019-06-17T19:50:02-07:00 Monday [America/Los_Angeles]
+ *   Eastern Time: 2019-06-17T22:50:02-04:00 Monday [America/New_York]
+ *
+ *   Pacific Time: 2019-06-17T19:50:04-07:00 Monday [America/Los_Angeles]
+ *   Eastern Time: 2019-06-17T22:50:04-04:00 Monday [America/New_York]
+ *   ...
+ */
+
+#include <AceRoutine.h>
+#include <AceTime.h>
+
+using namespace ace_time;
+using namespace ace_time::provider;
+using namespace ace_routine;
+
+// ZoneSpecifier instances should be created statically at initialization time.
+static BasicZoneSpecifier pacificSpec(&zonedb::kZoneAmerica_Los_Angeles);
+static BasicZoneSpecifier easternSpec(&zonedb::kZoneAmerica_New_York);
+
+SystemClock systemClock(nullptr /*sync*/, nullptr /*backup*/);
+SystemClockHeartbeatCoroutine systemClockHeartbeat(systemClock);
+
+//------------------------------------------------------------------
+
+void setup() {
+  delay(1000);
+  Serial.begin(115200); // ESP8266 default of 74880 not supported on Linux
+  while (!Serial); // Wait until Serial is ready - Leonardo/Micro
+
+  systemClock.setup();
+
+  // Creating timezones is cheap, so we can create them on the fly as needed.
+	auto pacificTz = TimeZone::forZoneSpecifier(&pacificSpec);
+
+  // Set the SystemClock using these components.
+  auto pacificTime = ZonedDateTime::forComponents(
+      2019, 6, 17, 19, 50, 0, pacificTz);
+  systemClock.setNow(pacificTime.toEpochSeconds());
+
+  systemClockHeartbeat.setupCoroutine(F("systemClockHeartbeat"));
+  CoroutineScheduler::setup();
+}
+
+//------------------------------------------------------------------
+
+void printCurrentTime() {
+  // Create Pacific Time from epoch seconds
+	auto pacificTz = TimeZone::forZoneSpecifier(&pacificSpec);
+  auto pacificTime = ZonedDateTime::forEpochSeconds(
+      systemClock.getNow(), pacificTz);
+
+  Serial.print(F("Pacific Time: "));
+  pacificTime.printTo(Serial);
+  Serial.println();
+
+  // Convert to Eastern Time.
+	auto easternTz = TimeZone::forZoneSpecifier(&easternSpec);
+  auto easternTime = pacificTime.convertToTimeZone(easternTz);
+
+  Serial.print(F("Eastern Time: "));
+  easternTime.printTo(Serial);
+  Serial.println();
+
+  Serial.println();
+}
+
+COROUTINE(print) {
+  COROUTINE_LOOP() {
+    printCurrentTime();
+    COROUTINE_DELAY(2000);
+  }
+}
+
+void loop() {
+  CoroutineScheduler::loop();
+}
