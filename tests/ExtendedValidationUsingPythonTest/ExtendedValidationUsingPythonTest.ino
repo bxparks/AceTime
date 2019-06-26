a2ad29aa77c646678244e35e1fbf54a83c71c732
Brian Park <brian@xparks.net>
2019-06-26 00:54:24 -0700

    tests: Rename ExtendedValidationTest to ExtendedValiationUsingPythonTest

---
+++ tests/ExtendedValidationUsingPythonTest/ExtendedValidationUsingPythonTest.ino
@@ -0,0 +1,36 @@
+#line 2 "ExtendedValidationUsingPythonTest.ino"
+
+/*
+ * This unit test depends on 'validation_data.cpp' which is so large that
+ * it will likely not compile on an Arduino environment. It can however be run
+ * on a Linux or MacOS environment using the provided Makefile.
+ */
+
+#include <AUnit.h>
+#include <AceTime.h>
+
+using namespace aunit;
+using namespace ace_time;
+
+// --------------------------------------------------------------------------
+
+void setup() {
+#if defined(ARDUINO)
+  delay(1000); // wait for stability on some boards to prevent garbage Serial
+#endif
+  Serial.begin(115200); // ESP8266 default of 74880 not supported on Linux
+  while(!Serial); // for the Arduino Leonardo/Micro only
+
+#if 0
+  TestRunner::exclude("*");
+  TestRunner::include("TransitionTest", "Gaza");
+  TestRunner::include("TransitionTest", "Goose_Bay");
+  TestRunner::include("TransitionTest", "Hebron");
+  TestRunner::include("TransitionTest", "Moncton");
+  TestRunner::include("TransitionTest", "St_Johns");
+#endif
+}
+
+void loop() {
+  TestRunner::run();
+}
