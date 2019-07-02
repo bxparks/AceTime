618e4598d0c2be7351be1a87e718aaa6e6858762
Brian Park <brian@xparks.net>
2019-07-02 15:46:04 -0700

    src/BasicZoneSpecifier: Handle {dayOfWeek}<={dayOfMonth} expressions, including shifts into prev and next months (but not year)

--- src/ace_time/BasicZoneSpecifier.h
+++ src/ace_time/BasicZoneSpecifier.h
@@ -276,26 +276,45 @@ class BasicZoneSpecifier: public ZoneSpecifier {
 		 * onDayOfWeek=1-7, onDayOfMonth=-(1-31): dayOfWeek<=dayOfMonth
 		 * @endverbatim
      *
-     * This method handles expressions which crosses month boundaries, but not
-     * year boundaries (e.g. Jan to Dec of the previous year, or Dec to Jan of
-     * the following year.)
+     * Caveats: This method handles expressions which crosses month boundaries,
+     * but not year boundaries (e.g. Jan to Dec of the previous year, or Dec to
+     * Jan of the following year.)
      *
      * Not private, used by ExtendedZoneSpecifier.
      */
     static basic::MonthDay calcStartDayOfMonth(int16_t year, uint8_t month,
-        uint8_t onDayOfWeek, uint8_t onDayOfMonth) {
-      if (onDayOfWeek == 0) return {month, onDayOfMonth};
+        uint8_t onDayOfWeek, int8_t onDayOfMonth) {
+      if (onDayOfWeek == 0) return {month, (uint8_t) onDayOfMonth};
 
-      // Convert "last{Xxx}" to "last{Xxx}>={daysInMonth-6}".
-      if (onDayOfMonth == 0) {
-        onDayOfMonth = LocalDate::daysInMonth(year, month) - 6;
+      if (onDayOfMonth >= 0) {
+        // Convert "last{Xxx}" to "last{Xxx}>={daysInMonth-6}".
+        uint8_t daysInMonth = LocalDate::daysInMonth(year, month);
+        if (onDayOfMonth == 0) {
+          onDayOfMonth =  daysInMonth - 6;
+        }
+
+        auto limitDate = LocalDate::forComponents(year, month, onDayOfMonth);
+        uint8_t dayOfWeekShift = (onDayOfWeek - limitDate.dayOfWeek() + 7) % 7;
+        uint8_t day = (uint8_t) (onDayOfMonth + dayOfWeekShift);
+        if (day > daysInMonth) {
+          // TODO: Support shifting from Dec to Jan of following  year.
+          day -= daysInMonth;
+          month++;
+        }
+        return {month, day};
+      } else {
+        onDayOfMonth = -onDayOfMonth;
+        auto limitDate = LocalDate::forComponents(year, month, onDayOfMonth);
+        int8_t dayOfWeekShift = (limitDate.dayOfWeek() - onDayOfWeek + 7) % 7;
+        int8_t day = onDayOfMonth - dayOfWeekShift;
+        if (day < 1) {
+          // TODO: Support shifting from Jan to Dec of the previous year.
+          month--;
+          uint8_t daysInPrevMonth = LocalDate::daysInMonth(year, month);
+          day += daysInPrevMonth;
+        }
+        return {month, (uint8_t) day};
       }
-
-      LocalDate limitDate = LocalDate::forComponents(
-          year, month, onDayOfMonth);
-      uint8_t dayOfWeekShift = (onDayOfWeek - limitDate.dayOfWeek() + 7) % 7;
-      uint8_t day = (onDayOfMonth + dayOfWeekShift);
-      return {month, day};
     }
 
   private:
