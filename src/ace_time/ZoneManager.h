6dd9e06b11f40ad3398d896a51f6389bb30269e8
Brian Park <brian@xparks.net>
2019-07-03 22:30:25 -0700

    src/ZoneManager: Use binarySearch() if registry is sorted and > 5 elements

--- src/ace_time/ZoneManager.h
+++ src/ace_time/ZoneManager.h
@@ -9,6 +9,10 @@
 #include <stdint.h>
 #include "common/common.h"
 
+class ZoneManagerTest_Basic_isSorted;
+class ZoneManagerTest_Basic_linearSearch;
+class ZoneManagerTest_Basic_binarySearch;
+
 namespace ace_time {
 
 /**
@@ -21,7 +25,8 @@ class ZoneManager {
     /** Constructor. */
     ZoneManager(const ZI* const* zoneRegistry, uint16_t registrySize):
         mZoneRegistry(zoneRegistry),
-        mRegistrySize(registrySize) {}
+        mRegistrySize(registrySize),
+        mIsSorted(isSorted(zoneRegistry, registrySize)) {}
 
     /**
      * Return the ZoneSpecifier corresponding to the given zone name. Return
@@ -29,22 +34,81 @@ class ZoneManager {
      * buffer.
      */
     const ZI* getZoneInfo(const char* name) const {
-      return linearSearch(name);
+      if (mIsSorted && mRegistrySize > kBinarySearchThreshold) {
+        return binarySearch(mZoneRegistry, mRegistrySize, name);
+      } else {
+        return linearSearch(mZoneRegistry, mRegistrySize, name);
+      }
     }
 
+    /**
+     * Return true if zoneRegistry is sorted, and eligible to use a binary
+     * search.
+     */
+    bool isSorted() const { return mIsSorted; }
+
   private:
-    const ZI* linearSearch(const char* name) const {
-      for (uint16_t i = 0; i < mRegistrySize; ++i) {
-        const ZI* zoneInfo = mZoneRegistry[i];
-        if (strcmp(zoneInfo->name, name) == 0) {
+    friend class ::ZoneManagerTest_Basic_isSorted;
+    friend class ::ZoneManagerTest_Basic_linearSearch;
+    friend class ::ZoneManagerTest_Basic_binarySearch;
+
+    /** Use binarySearch() if registrySize > threshold. */
+    static const uint8_t kBinarySearchThreshold = 5;
+
+    static bool isSorted(const ZI* const* zoneRegistry, uint16_t registrySize) {
+      if (registrySize == 0) {
+        return false;
+      }
+      const char* prevName = zoneRegistry[0]->name;
+      for (uint16_t i = 1; i < registrySize; ++i) {
+        const char* currName = zoneRegistry[i]->name;
+        if (strcmp(prevName, currName) > 0) {
+          return false;
+        }
+        prevName = currName;
+      }
+      return true;
+    }
+
+    static const ZI* linearSearch(const ZI* const* zoneRegistry, uint16_t registrySize,
+        const char* name) {
+      for (uint16_t i = 0; i < registrySize; ++i) {
+        const ZI* zoneInfo = zoneRegistry[i];
+        if (strcmp(name, zoneInfo->name) == 0) {
           return zoneInfo;
         }
       }
       return nullptr;
     }
 
+    static const ZI* binarySearch(const ZI* const* zoneRegistry, uint16_t registrySize,
+        const char* name) {
+      uint16_t a = 0;
+      uint16_t b = registrySize - 1;
+      while (b - a > 0) {
+        uint16_t i = (a + b) / 2;
+        const ZI* zi = zoneRegistry[i];
+        int8_t compare = strcmp(name, zi->name);
+        if (compare < 0) {
+          b = i - 1;
+        } else if (compare > 0) {
+          a = i + 1;
+        } else {
+          return zi;
+        }
+      }
+
+      const ZI* zi = zoneRegistry[a];
+      if (strcmp(name, zi->name) == 0) {
+        return zi;
+      } else {
+        return nullptr;
+      }
+    }
+
     const ZI* const* const mZoneRegistry;
     uint16_t const mRegistrySize;
+    bool const mIsSorted;
 };
 
 }
--- tests/ZoneManagerTest/ZoneManagerTest.ino
+++ tests/ZoneManagerTest/ZoneManagerTest.ino
@@ -13,6 +13,8 @@ using namespace ace_time;
 test(ZoneManagerTest_Basic, getZoneInfo_Los_Angeles) {
   ZoneManager<basic::ZoneInfo> zoneManager(
       zonedb::kZoneRegistry, zonedb::kZoneRegistrySize);
+  assertTrue(zoneManager.isSorted());
+
   const basic::ZoneInfo* zoneInfo =
       zoneManager.getZoneInfo("America/Los_Angeles");
   assertTrue(zoneInfo != nullptr);
@@ -34,6 +36,8 @@ test(ZoneManagerTest_Basic, getZoneInfo_not_found) {
 test(ZoneManagerTest_Extended, getZoneInfo_Los_Angeles) {
   ZoneManager<extended::ZoneInfo> zoneManager(
       zonedbx::kZoneRegistry, zonedbx::kZoneRegistrySize);
+  assertTrue(zoneManager.isSorted());
+
   const extended::ZoneInfo* zoneInfo =
       zoneManager.getZoneInfo("America/Los_Angeles");
   assertTrue(zoneInfo != nullptr);
@@ -46,6 +50,35 @@ test(ZoneManagerTest_Extended, getZoneInfo_not_found) {
   const extended::ZoneInfo* zoneInfo =
       zoneManager.getZoneInfo("not found");
   assertTrue(zoneInfo == nullptr);
+}
+
+// --------------------------------------------------------------------------
+// ZoneManager::isSorted(), binarySearch(), linearSearch()
+// --------------------------------------------------------------------------
+
+const basic::ZoneInfo* const kRegistry[] = {
+  &zonedb::kZoneAmerica_Chicago,
+  &zonedb::kZoneAmerica_Denver,
+  &zonedb::kZoneAmerica_Los_Angeles,
+  &zonedb::kZoneAmerica_New_York,
+};
+
+test(ZoneManagerTest_Basic, isSorted) {
+  bool isSorted = ZoneManager<basic::ZoneInfo>::isSorted(
+      kRegistry, sizeof(kRegistry)/sizeof(basic::ZoneInfo*));
+  assertTrue(isSorted);
+}
+
+test(ZoneManagerTest_Basic, linearSearch) {
+  const basic::ZoneInfo *zi = ZoneManager<basic::ZoneInfo>::linearSearch(
+      kRegistry, sizeof(kRegistry)/sizeof(basic::ZoneInfo*), "America/Los_Angeles");
+  assertEqual(zi->name, "America/Los_Angeles");
+}
+
+test(ZoneManagerTest_Basic, binarySearch) {
+  const basic::ZoneInfo *zi = ZoneManager<basic::ZoneInfo>::binarySearch(
+      kRegistry, sizeof(kRegistry)/sizeof(basic::ZoneInfo*), "America/Los_Angeles");
+  assertEqual(zi->name, "America/Los_Angeles");
 }
 
 // --------------------------------------------------------------------------
