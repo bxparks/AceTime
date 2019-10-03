e40a6ad6c00a1a68adc76ef75479b0074d154b8a
Brian Park <brian@xparks.net>
2019-10-02 20:54:49 -0700

    docs: Regenerate doxygen docs for v1.0

--- docs/html/search/classes_b.js
+++ docs/html/search/classes_b.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['yearmonth',['YearMonth',['../structace__time_1_1basic_1_1YearMonth.html',1,'ace_time::basic']]],
   ['yearmonthtuple',['YearMonthTuple',['../structace__time_1_1extended_1_1YearMonthTuple.html',1,'ace_time::extended']]]
 ];
