ab261b8144ea847cd8dbfe322fe124af4ea60745
Anatoli Arkhipenko <arkhipenko@hotmail.com>
2020-12-08 19:05:07 -0500

    Support for STM32 architecture + STM RTC clock (take#2) (#39)
    * Support for STM32 architecture + STM RTC clock
    
    * fixing cr/lf

---
+++ src/ace_time/clock/StmRtcClock.h
@@ -0,0 +1,79 @@
+/*
+ * MIT License
+ * Copyright (c) 2018 Brian T. Park, Anatoli Arkhipenko
+ 
+ Requires https://github.com/stm32duino/STM32RTC
+ */
+
+#ifndef ACE_TIME_STMRTC_CLOCK_H
+#define ACE_TIME_STMRTC_CLOCK_H
+
+#if ! defined(UNIX_HOST_DUINO)
+#if defined(ARDUINO_ARCH_STM32)
+
+#include <stdint.h>
+#include "../hw/StmRtc.h"
+#include "../hw/HardwareDateTime.h"
+#include "../LocalDateTime.h"
+#include "Clock.h"
+
+namespace ace_time {
+namespace clock {
+
+/**
+ * An implementation of Clock that uses a STM32 RTC chip.
+ */
+class StmRtcClock: public Clock {
+  public:
+    explicit StmRtcClock() {}
+
+    void setup() {}
+
+    acetime_t getNow() const override {
+      hw::HardwareDateTime hardwareDateTime;
+      mStmRtc.readDateTime(&hardwareDateTime);
+      return toDateTime(hardwareDateTime).toEpochSeconds();
+    }
+    
+    void setNow(acetime_t epochSeconds) override {
+      if (epochSeconds == kInvalidSeconds) return;
+
+      LocalDateTime now = LocalDateTime::forEpochSeconds(epochSeconds);
+      mStmRtc.setDateTime(toHardwareDateTime(now));
+    }
+
+    bool isTimeSet() const { 
+      return mStmRtc.isTimeSet(); 
+    }
+    
+  private:
+    /**
+     * Convert the HardwareDateTime returned by the STM RTC chip to
+     * the LocalDateTime object using UTC time zone.
+     */
+    static LocalDateTime toDateTime(const hw::HardwareDateTime& dt) {
+      return LocalDateTime::forComponents(
+          dt.year + LocalDate::kEpochYear, dt.month, dt.day,
+          dt.hour, dt.minute, dt.second);
+    }
+
+    /**
+     * Convert a LocalDateTime object to a HardwareDateTime object, ignoring
+     * time zone. In practice, it will often be most convenient to store
+     * as UTC time. Only 2 digits are supported by the year field.
+     */
+    static hw::HardwareDateTime toHardwareDateTime(const LocalDateTime& dt) {
+      return hw::HardwareDateTime{(uint8_t) dt.yearTiny(), dt.month(),
+          dt.day(), dt.hour(), dt.minute(), dt.second(), dt.dayOfWeek()};
+    }
+
+    const hw::StmRtc mStmRtc;
+};
+
+}
+}
+#endif  //  #if defined(ARDUINO_ARCH_STM32)
+
+#endif
+
+#endif  //  ACE_TIME_STMRTC_CLOCK_H
