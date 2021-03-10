356b60a713047016d4aca89cfedb2d381ff16b2a
Brian Park <brian@xparks.net>
2021-03-09 16:40:43 -0800

    tools/compare_noda: Initial console app with Noda Time

---
+++ tools/compare_noda/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace compare_noda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello compare_noda!");
+        }
+    }
+}
