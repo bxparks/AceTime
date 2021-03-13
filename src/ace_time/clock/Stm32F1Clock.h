f48603d7275f770a335d1df01843ee4cced9393d
Brian Park <brian@xparks.net>
2021-03-13 11:47:30 -0800

    src/Stm32F1Rtc.h,Stm32F1Clock.h: Add extensive doxygen docstrings to explain how these classes are specialized for the STM32F1 chip

--- src/ace_time/clock/Stm32F1Clock.h
+++ src/ace_time/clock/Stm32F1Clock.h
@@ -1,11 +1,6 @@
 /*
  * MIT License
  * Copyright (c) 2021 Brian T. Park
- *
- * A clock that uses the LSE_CLOCK (Low Speed External) on the STM32F1xx which
- * keeps updating the 32-bit RTC counter as long as the battery is attached to
- * VBat. We use STM32F1_RTC to write directly to the RTC_CNTH and RTC_CNTL
- * registers.
  */
 
 #ifndef ACE_TIME_STM32_F1_CLOCK_H
@@ -22,8 +17,52 @@ namespace ace_time {
 namespace clock {
 
 /**
- * An implementation of Clock that uses the LSE_CLOCK (Low Speed External)
- * clock on the STM32F1 RTC chip.
+ * An implementation of `Clock` that is specialized for the LSE_CLOCK (Low Speed
+ * External clock) on the STM32F1 RTC chip. Normally, the LSE_CLOCK requires an
+ * additional external 32.768 kHz crystal, but the popular "Blue Pill" dev board
+ * already includes this extenal crystal on pins C14 and C15. **Warning**: For
+ * the highest clock accuracy, those pins should not be attached anything else,
+ * not even the male header pins. The header pins will add too much stray
+ * capacitance to the oscillator circuit, and cause the clock to run too slow. I
+ * have seen the clock run as much as 10% too slow with the male header pins
+ * attached. If you hold a finger to those pins, it adds so much capacitance
+ * that the LSE_CLOCK will appear to just stop.
+ *
+ * There are 3 possible RTC clocks on the STM32F1 (HSI_CLOCK, LSI_CLOCK, and
+ * LSE_CLOCK). But the LSE_CLOCK is special because it keeps updating the RTC on
+ * the STM32F1 through a reset or power loss, as long as a battery is attached
+ * to VBat. The battery could be a 3V CR2032 coin battery, or 2AA rechargeable
+ * or non-rechargeable battery, or it could be a super capacitor.
+ *
+ * This class uses the `Stm32F1Rtc` helper class to write directly to the RTC
+ * registers on the STM32F1, bypassing the generic `STM32RTC` library
+ * (https://github.com/stm32duino/STM32RTC) which would normally be used for
+ * other STM32 microcontrollers. The generic `STM32RTC` library
+ * has a bug on the STM32F1 where it preserves only the time fields in the RTC
+ * registers, saving the date fields on SRAM which is lost upon reset (See
+ * https://github.com/stm32duino/STM32RTC/issues/29 and
+ * https://github.com/stm32duino/STM32RTC/issues/32). The problem is caused in
+ * the low-level HAL (hardware abstraction layer) of the STM32F1 chip, because
+ * unlike other STM32 processors which stores the time and date fields as
+ * separate fields, the RTC on the STM32F1 is just a simple 32-bit counter
+ * (split across 2 registeres, RTC_CNTH and RTC_CNTL) that increments once a
+ * second.
+ *
+ * It turns out that for the purposes of AceTime and the `SystemClock`, a 32-bit
+ * counter is sufficient to support all of its functionality. In particular, the
+ * 32-bit counter is sufficient to allow AceTime to retain both date and time
+ * fields through a power reset. So the `Stm32F1Rtc` class is a narrowly
+ * targeted HAL whose only purpose is to read from and write to the 32-bit RTC
+ * counter on the STM32F1 chip. It bypasses the entire generic RTC and HAL
+ * layers provided by the STM32duino framework.
+ *
+ * The `Stm32F1Rtc` class uses one additional register, the Backup DR1 register,
+ * which holds a single status bit to indicate whether or not the underlying RTC
+ * counter has been initialized to a valid time. The selection of the `DR1`
+ * register, instead of any of the other 9-10 backup registers, is currently
+ * hardcoded in `Stm32F1Rtc.h`. If that causes a conflict with something else,
+ * let me know, because this is fixable. We can make that a configurable
+ * parameter in the Stm32F1Rtc::begin() method.
  */
 class Stm32F1Clock: public Clock {
   public:
@@ -46,7 +85,7 @@ class Stm32F1Clock: public Clock {
     mutable hw::Stm32F1Rtc mStm32F1Rtc;
 };
 
-} // hw
+} // clock
 } // ace_time
 
 #endif // #if defined(STM32F1xx)
--- src/ace_time/hw/Stm32F1Rtc.h
+++ src/ace_time/hw/Stm32F1Rtc.h
@@ -3,12 +3,15 @@
  * Copyright (c) 2021 Brian T. Park
  *
  * Extracted from https://github.com/ZulNs/STM32F1_RTC/
+ * MIT License
  * Copyright (c) 2019 ZulNs
  */
 
-#ifndef STM32_F1_RTC_H
-#define STM32_F1_RTC_H
+#ifndef ACE_TIME_STM32_F1_RTC_H
+#define ACE_TIME_STM32_F1_RTC_H
 
+// This class works ONLY on the STM32F1, whose most common board is probably the
+// "Blue Pill".
 #if defined(STM32F1xx)
 
 #include <Arduino.h> // RTC, RCC, PWR, BKP
@@ -24,7 +27,17 @@
 #define RCC_BDCR    RCC->BDCR
 #define PWR_CR      PWR->CR
 
+// Set the RTC_INIT_REG to to hold the 'init' bit, indicating that the RTC
+// counter has been set to a valid epochSeconds value.
 //
+// If the default DR1 register causes conflicts with some other library, we can
+// make this a configurable parameter in the begin() method. But that would
+// require a bit hacking. Currently, the DR registers are accessible only
+// through the `BKP` pointer which points to the `BKP_TypeDef` struct. If we
+// want to refer to the backup registers using a numeric index (e.g. 1, 2), we
+// would need to do some pointer casting. In other words, cast the `BKP` to a
+// `__IO uint32_t* drp = (__IO uint32*) BKP`, then use `drp[index]` to get to
+// the specific `DR{index}` register.
 #define RTC_INIT_REG  BKP->DR1
 #define RTC_INIT_BIT  0
 #define RTC_INIT_FLAG (1 << RTC_INIT_BIT)
@@ -36,14 +49,14 @@ namespace hw {
 
 /**
  * A thin abstraction above the RTC_CNTL and RTC_CNTH registers which are in
- * the "backup domain". When powered by LSE_CLOCK (Low Speed External) the
- * counter continues to count as long as the VBat is powered.
+ * the "backup domain" of the STM32F1 chip. When powered by LSE_CLOCK (Low Speed
+ * External) the counter continues to count as long as the VBat is powered.
  *
- * The STM32RTC library (https://github.com/stm32duino/STM32RTC) uses SRAM on
- * STM32F1 to hold the date fields, which means that the date fields are not
- * preserved during power loss. This class completely bypasses the HAL (hardware
- * abstarction layer) and writes the 32-bit epochSeconds quantity directly into
- * the `RTC->CNTH` and `RTC->CNTL` registers.
+ * The generic STM32RTC library (https://github.com/stm32duino/STM32RTC) uses
+ * SRAM on STM32F1 to hold the date fields, which means that the date fields are
+ * not preserved during power loss. This class completely bypasses the HAL
+ * (hardware abstarction layer) and writes the 32-bit epochSeconds quantity
+ * directly into the `RTC->CNTH` and `RTC->CNTL` registers.
  *
  * The Backup DR2 register is used to hold a single bit, indicating whether or
  * not the RTC has been initialized.
