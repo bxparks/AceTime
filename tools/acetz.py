77f1134d0c98f6514ce8d89b949fd6f452f30bb8
Brian Park <brian@xparks.net>
2021-03-19 10:33:29 -0700

    tools/acetz.py: Remove debug print()

--- tools/acetz.py
+++ tools/acetz.py
@@ -10,7 +10,7 @@ from data_types.at_types import SECONDS_SINCE_UNIX_EPOCH
 from zone_processor.zone_specifier import ZoneSpecifier
 from zone_processor.inline_zone_info import ZoneInfo
 
-__version__ = '1.1'
+__version__ = '0.1'
 
 
 class acetz(tzinfo):
@@ -31,18 +31,6 @@ class acetz(tzinfo):
                 f'{dt.year:04}-{dt.month:02}-{dt.day:02} '
                 f'{dt.hour:02}:{dt.minute:02}:{dt.second:02}'
             )
-
-        # print("utcoffset(): start traceback", file=sys.stderr)
-        # traceback.print_stack()
-        # print("utcoffset(): end traceback", file=sys.stderr)
-        # print(f"acetz.utcoffset(): dt={dt.date()} {dt.time()} fold={dt.fold}")
-        # print(
-        #     f"acetz.utcoffset(): info: "
-        #     f"utc_offset={info.utc_offset} "
-        #     f"dst_offset={info.dst_offset} "
-        #     f"abbrev={info.abbrev} "
-        #     f"fold={info.fold}"
-        # )
 
         return timedelta(seconds=info.total_offset)
 
@@ -84,37 +72,22 @@ class acetz(tzinfo):
             raise TypeError("fromutc() requires a datetime argument")
         if dt.tzinfo is not self:
             raise ValueError("dt.tzinfo is not self")
-        # print(f"acetz.fromutc(): dt={dt.date()} {dt.time()}")
-
-        # print("utcoffset(): start traceback", file=sys.stderr)
-        # traceback.print_stack()
-        # print("utcoffset(): end traceback", file=sys.stderr)
 
         # Extract the epoch_seconds of the source 'dt'
         assert dt is not None
         utcdt = dt.replace(tzinfo=timezone.utc)
-        # print(f"acetz.fromutc(): utcdt={utcdt.date()} {utcdt.time()}")
         unix_seconds = int(utcdt.timestamp())
         epoch_seconds = unix_seconds - SECONDS_SINCE_UNIX_EPOCH
-        # print(f"acetz.fromutc(): utcdt.epoch_seconds={epoch_seconds}")
 
         # Search the transitions for the matching Transition
         offset_info = self.zs.get_timezone_info_for_seconds(epoch_seconds)
         if not offset_info:
-            raise ValueError("transition not found")
+            raise ValueError(f"transition not found for {epoch_seconds}")
 
+        # Convert the date/time fields into local date/time and attach
+        # the current acetz object.
         newutcdt = utcdt + timedelta(seconds=offset_info.total_offset)
-        # print(
-        #    f"acetz.fromutc(): "
-        #    f"newutcdt={newutcdt.date()} {newutcdt.time()}"
-        # )
-
         newdt = newutcdt.replace(tzinfo=self, fold=offset_info.fold)
-        # print(f"acetz.fromutc(): newdt={newdt.date()} {newdt.time()}")
-        # print(f"acetz.fromutc(): newdt.utcoffset={newdt.utcoffset()}")
-        # print(f"acetz.fromutc(): newdt.dst={newdt.dst()}")
-        epoch_seconds = int(newdt.timestamp() - SECONDS_SINCE_UNIX_EPOCH)
-        # print(f"acetz.fromutc(): newdt.epoch_seconds={epoch_seconds}")
 
         return newdt
 
