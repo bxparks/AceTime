b34acd2ff2f027a0b66e585709623d7c7eb94296
Brian Park <brian@xparks.net>
2022-02-14 13:19:00 -0800

    ComparisonBenchmark: Generate *.txt files and process them into README.md using generator scripts (like AutoBenchmark and MemoryBenchmark)

--- examples/ComparisonBenchmark/ComparisonBenchmark.ino
+++ examples/ComparisonBenchmark/ComparisonBenchmark.ino
@@ -1,13 +1,18 @@
 /*
- * Determine the execution time of a round trip conversion from human-readable
- * date/time components, to epochSeconds, then back to human-readable
- * components. We compare this round trip for the AceTime library compared to
- * the Arduino Time library (https://github.com/PaulStoffregen/Time).
+ * Determine the execution time of functions which convert human-readable
+ * date/time components to and from epochSeconds. We compare the equivalent
+ * methods from the AceTime library to the Arduino Time library
+ * (https://github.com/PaulStoffregen/Time).
  *
  * The Arduino Time library does not compile on EpoxyDuino.
  */
 
 #include "Benchmark.h"
+
+// ESP32 does not define SERIAL_PORT_MONITOR
+#if !defined(SERIAL_PORT_MONITOR)
+#define SERIAL_PORT_MONITOR Serial
+#endif
 
 void setup() {
   delay(1000);
@@ -15,7 +20,9 @@ void setup() {
   SERIAL_PORT_MONITOR.begin(115200);
   while (!SERIAL_PORT_MONITOR); // Wait until ready - Leonardo/Micro
 
+  SERIAL_PORT_MONITOR.println(F("BENCHMARKS"));
   runBenchmarks();
+  SERIAL_PORT_MONITOR.println(F("END"));
 }
 
 void loop() {}
