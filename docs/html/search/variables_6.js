0c8d2c2145ba6962ae7392336182e4a90b45c88f
Brian Park <brian@xparks.net>
2022-08-13 18:19:41 -0700

    docs: Regenerate doxygen for 1.11.4 (TZDB 2022b)

--- docs/html/search/variables_6.js
+++ docs/html/search/variables_6.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['lastdeltaminutes_571',['lastDeltaMinutes',['../structace__time_1_1extended_1_1MatchingEraTemplate.html#ad68b2ed172f4490432fa3aba43ca3b8e',1,'ace_time::extended::MatchingEraTemplate']]],
-  ['lastoffsetminutes_572',['lastOffsetMinutes',['../structace__time_1_1extended_1_1MatchingEraTemplate.html#a62ca52350ec0d6e122e166003bbf19c7',1,'ace_time::extended::MatchingEraTemplate']]],
+  ['lastdeltaminutes_571',['lastDeltaMinutes',['../structace__time_1_1extended_1_1MatchingEraTemplate.html#a54213b73ec7e1341d4af32147566713b',1,'ace_time::extended::MatchingEraTemplate']]],
+  ['lastoffsetminutes_572',['lastOffsetMinutes',['../structace__time_1_1extended_1_1MatchingEraTemplate.html#ad6e089ef39f3531fae3c27705f1fc0aa',1,'ace_time::extended::MatchingEraTemplate']]],
   ['letterbuf_573',['letterBuf',['../structace__time_1_1extended_1_1TransitionTemplate.html#a9609afcd25072766cff26ea70ea68422',1,'ace_time::extended::TransitionTemplate']]]
 ];
