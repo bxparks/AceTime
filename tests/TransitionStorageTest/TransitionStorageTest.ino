dc437ef7b9ecd9ab30b1f6ddc21152899abe541e
Brian Park <brian@xparks.net>
2023-01-13 13:36:49 -0800

    ExtendedZoneProcessorTest: Move DateTuple tests into TransitionStorageTest

--- tests/TransitionStorageTest/TransitionStorageTest.ino
+++ tests/TransitionStorageTest/TransitionStorageTest.ino
@@ -1,8 +1,7 @@
 #line 2 "TransitionStorageTest.ino"
 
 /*
- * Unit tests for TransitionStorage, extracted from ExtendedZoneProcessorTest
- * after it became too big to run on an Arduino/SparkFun ProMicro environment.
+ * Unit tests for Transition and TransitionStorage.
  */
 
 #include <AUnit.h>
@@ -13,6 +12,95 @@ using namespace ace_time::extended;
 using ace_time::internal::ZoneContext;
 
 //---------------------------------------------------------------------------
+// DateTuple.
+//---------------------------------------------------------------------------
+
+test(TransitionStorage, dateTupleOperatorLessThan) {
+  assertTrue((
+      DateTuple{2000, 1, 2, 3, ZoneContext::kSuffixW}
+      < DateTuple{2000, 1, 2, 4, ZoneContext::kSuffixS}));
+  assertTrue((
+      DateTuple{2000, 1, 2, 3, ZoneContext::kSuffixW}
+      < DateTuple{2000, 1, 3, 3, ZoneContext::kSuffixS}));
+  assertTrue((
+      DateTuple{2000, 1, 2, 3, ZoneContext::kSuffixW}
+      < DateTuple{2000, 2, 2, 3, ZoneContext::kSuffixS}));
+  assertTrue((
+      DateTuple{2000, 1, 2, 3, ZoneContext::kSuffixW}
+      < DateTuple{2001, 1, 2, 3, ZoneContext::kSuffixS}));
+}
+
+test(TransitionStorage, dateTupleOperatorEquals) {
+  assertTrue((
+      DateTuple{2000, 1, 2, 3, ZoneContext::kSuffixW}
+      == DateTuple{2000, 1, 2, 3, ZoneContext::kSuffixW}));
+
+  assertFalse((
+      DateTuple{2000, 1, 2, 3, ZoneContext::kSuffixW}
+      == DateTuple{2000, 1, 2, 3, ZoneContext::kSuffixS}));
+  assertFalse((
+      DateTuple{2000, 1, 2, 3, ZoneContext::kSuffixW}
+      == DateTuple{2000, 1, 2, 4, ZoneContext::kSuffixW}));
+  assertFalse((
+      DateTuple{2000, 1, 2, 3, ZoneContext::kSuffixW}
+      == DateTuple{2000, 1, 3, 3, ZoneContext::kSuffixW}));
+  assertFalse((
+      DateTuple{2000, 1, 2, 3, ZoneContext::kSuffixW}
+      == DateTuple{2000, 2, 2, 3, ZoneContext::kSuffixW}));
+  assertFalse((
+      DateTuple{2000, 1, 2, 3, ZoneContext::kSuffixW}
+      == DateTuple{2001, 1, 2, 3, ZoneContext::kSuffixW}));
+}
+
+test(TransitionStorage, normalizeDateTuple) {
+  DateTuple dtp;
+
+  dtp = {2000, 1, 1, 0, ZoneContext::kSuffixW};
+  normalizeDateTuple(&dtp);
+  assertTrue((dtp == DateTuple{2000, 1, 1, 0, ZoneContext::kSuffixW}));
+
+  dtp = {2000, 1, 1, 15*95, ZoneContext::kSuffixW}; // 23:45
+  normalizeDateTuple(&dtp);
+  assertTrue((dtp == DateTuple{2000, 1, 1, 15*95, ZoneContext::kSuffixW}));
+
+  dtp = {2000, 1, 1, 15*96, ZoneContext::kSuffixW}; // 24:00
+  normalizeDateTuple(&dtp);
+  assertTrue((dtp == DateTuple{2000, 1, 2, 0, ZoneContext::kSuffixW}));
+
+  dtp = {2000, 1, 1, 15*97, ZoneContext::kSuffixW}; // 24:15
+  normalizeDateTuple(&dtp);
+  assertTrue((dtp == DateTuple{2000, 1, 2, 15, ZoneContext::kSuffixW}));
+
+  dtp = {2000, 1, 1, -15*96, ZoneContext::kSuffixW}; // -24:00
+  normalizeDateTuple(&dtp);
+  assertTrue((dtp == DateTuple{1999, 12, 31, 0, ZoneContext::kSuffixW}));
+
+  dtp = {2000, 1, 1, -15*97, ZoneContext::kSuffixW}; // -24:15
+  normalizeDateTuple(&dtp);
+  assertTrue((dtp == DateTuple{1999, 12, 31, -15, ZoneContext::kSuffixW}));
+}
+
+test(TransitionStorage, substractDateTuple) {
+  DateTuple dta = {2000, 1, 1, 0, ZoneContext::kSuffixW}; // 2000-01-01 00:00
+  DateTuple dtb = {2000, 1, 1, 1, ZoneContext::kSuffixW}; // 2000-01-01 00:01
+  acetime_t diff = subtractDateTuple(dta, dtb);
+  assertEqual(-60, diff);
+
+  dta = {2000, 1, 1, 0, ZoneContext::kSuffixW}; // 2000-01-01 00:00
+  dtb = {2000, 1, 2, 0, ZoneContext::kSuffixW}; // 2000-01-02 00:00
+  diff = subtractDateTuple(dta, dtb);
+  assertEqual((int32_t) -86400, diff);
+
+  dta = {2000, 1, 1, 0, ZoneContext::kSuffixW}; // 2000-01-01 00:00
+  dtb = {2000, 2, 1, 0, ZoneContext::kSuffixW}; // 2000-02-01 00:00
+  diff = subtractDateTuple(dta, dtb);
+  assertEqual((int32_t) -86400 * 31, diff); // January has 31 days
+
+  dta = {2000, 2, 1, 0, ZoneContext::kSuffixW}; // 2000-02-01 00:00
+  dtb = {2000, 3, 1, 0, ZoneContext::kSuffixW}; // 2000-03-01 00:00
+  diff = subtractDateTuple(dta, dtb);
+  assertEqual((int32_t) -86400 * 29, diff); // Feb 2000 is leap, 29 days
+}
 
 test(TransitionStorageTest, compareDateTupleFuzzy) {
   using ace_time::extended::MatchStatus;
@@ -509,6 +597,9 @@ void setup() {
 #endif
   SERIAL_PORT_MONITOR.begin(115200);
   while (!SERIAL_PORT_MONITOR); // Leonardo/Micro
+#if defined(EPOXY_DUINO)
+  SERIAL_PORT_MONITOR.setLineModeUnix();
+#endif
 }
 
 void loop() {
