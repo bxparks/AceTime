12f7d287b6ff10b94f5e2fa8704e838c82f6af34
Brian Park <brian@xparks.net>
2023-06-04 18:48:29 -0700

    CompleteZoneManager: create also CompleteZone, CompleteRegistrar

--- src/ace_time/CompleteZone.cpp
+++ src/ace_time/CompleteZone.cpp
@@ -1,28 +1,28 @@
 /*
  * MIT License
- * Copyright (c) 2019 Brian T. Park
+ * Copyright (c) 2023 Brian T. Park
  */
 
 #include <Arduino.h>
 #include <AceCommon.h> // KString
 #include "../zoneinfo/BrokerCommon.h" // findShortName()
-#include "BasicZone.h"
+#include "CompleteZone.h"
 
 using ace_common::KString;
 using ace_common::printReplaceCharTo;
 using ace_time::internal::findShortName;
-using ace_time::basic::ZoneContextBroker;
+using ace_time::complete::ZoneContextBroker;
 
 namespace ace_time {
 
-void BasicZone::printNameTo(Print& printer) const {
+void CompleteZone::printNameTo(Print& printer) const {
   const __FlashStringHelper* name = mZoneInfoBroker.name();
   ZoneContextBroker zoneContext = mZoneInfoBroker.zoneContext();
   KString kname(name, zoneContext.fragments(), zoneContext.numFragments());
   kname.printTo(printer);
 }
 
-void BasicZone::printShortNameTo(Print& printer) const {
+void CompleteZone::printShortNameTo(Print& printer) const {
   const __FlashStringHelper* name = mZoneInfoBroker.name();
   const __FlashStringHelper* shortName = findShortName(name);
   printReplaceCharTo(printer, shortName, '_', ' ');
